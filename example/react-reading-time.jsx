35ccf8fa518e1ab4a7b3b533585a370d83de5c84
KwakesProject <kwakes@me.com>
2016-09-20 14:55:42 +0100

    section 2 completed

--- example/react-reading-time.jsx
+++ example/react-reading-time.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class ReactReadingTime extends React.Component {
-    render() {
-        return (
-            <div className='container'>
-                Hello React!
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='container'>
+        Hello React!
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<ReactReadingTime />, document.getElementById('react'))
